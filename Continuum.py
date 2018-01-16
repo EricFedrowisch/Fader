9480e71ad586f0e83eea22c3f8a1b3999053fee9
ArikVhedrovix <efedrowisch@yahoo.com>
2018-01-16 10:33:25 -0600

    Zone Partially Implemented. Ignores updated.

--- Continuum.py
+++ Continuum.py
@@ -4,11 +4,14 @@
 # Jan.13, 2018: Basic class outlines begun and high level logic stubbed out.
 #               Got time to run forward in a straight line.
 # Jan. 14, 2018: Got one backward hop then resuming forward flow to work.
-# Jan. 14, 2018: Got multiple backwards hops to work.
+# Jan. 14, 2018: Got multiple backwards hops to work. Forward too.
+
 # Container class for timeframes
 class Continuum():
-    def __init__(self):
+    def __init__(self, zone = None):
         self.timeframes = []
+        self.timeframes.append(Timeframe(self, 0)) #Create inital timeframe
+        self.timeframes[0].zone = zone
         self.advance_time = True
         self.focalTimeframe = 0 # Initially focus is on leading edge of present
         self.time_jaunt_length = 25 # How many turns max can you go back to from LEP
@@ -37,15 +40,15 @@ class Continuum():
         return n >= 0 and n < len(self.timeframes)
 
     def travelBack(self, turns):
-        print "Trying travel back " + str(turns) + " turns."
+        print "### Trying to travel back " + str(turns) + " turns. ###"
         if self.inBounds(turns):
             self.advance_time = False
             self.focalTimeframe += turns
         else:
-            print "Travel back failed. Out of bounds."
+            print "### Travel back failed. Out of bounds. ###"
 
     def travelForward(self, turns):
-        print "Trying travel forward " + str(turns) + " turns."
+        print "### Trying to travel forward " + str(turns) + " turns. ###"
         if self.inBounds(turns * -1):
             self.focalTimeframe -= turns
             if self.focalTimeframe > 0:
@@ -53,7 +56,7 @@ class Continuum():
             elif self.focalTimeframe == 0:
                 self.advance_time = True
         else:
-            print "Travel forward failed. Out of bounds."
+            print "### Travel forward failed. Out of bounds. ###"
 
 
     def nextTurn(self):
@@ -68,8 +71,6 @@ class Continuum():
                 PreviousFrame = self.timeframes[0]
                 NextFrame = Timeframe(self, (PreviousFrame.turn+1), PreviousFrame)
                 self.timeframes.insert(0, NextFrame) # Add one more timeframe
-            else:
-                self.timeframes.insert(0,Timeframe(self)) # Add first timeframe
             self.timeframes[self.focalTimeframe].play()
             self.leadingTurn +=1
             self.focalTimeframe = 0
@@ -84,35 +85,49 @@ class Continuum():
                 self.nextTurn() #Will this cause subtle bugs? I put it here to prevent dupe LEP turns, but not sure fixing one problem won't make others
 
 
-# Map and event container
+# zone and event container
 class Timeframe():
     def __init__(self, Continuum, turn, PreviousFrame = None):
         self.turn = turn
-        self.map = None
+        self.zone = None
         if PreviousFrame:
-            self.map = PreviousFrame.map
+            self.zone = PreviousFrame.zone
     def play(self):
         # Play/Replay turn, noting any changes, checking for collisons/paradoxes
         pass
 
-class Map():
-    def __init__(self):
-        pass
+class Zone():
+    def __init__(self, xSize = 20, ySize = 20):
+        self.zoneMap = []
+        for y in range(0, ySize):
+            for x in range(0, xSize):
+                self.zoneMap.append(Tile(x, y))
 
+class Tile():
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.contents = []
+
+# Exercise
 if __name__ == '__main__':
     c = Continuum()
-    t1 = Timeframe(c, 0)
-    c.timeframes.append(t1)
     c.state(verbose = True)
     print "#####After advancing 30 turns...######"
     for i in range(0,30):
         c.nextTurn()
     c.state(verbose = True)
-    '''
-    for i in range(0,5):
-        c.state()
-        c.nextTurn()
+    c.travelBack(15)
     for i in range(0,10):
         c.state()
         c.nextTurn()
-    '''
+    c.state(verbose = True)
+    c.travelForward(15)
+    c.state(verbose = True)
+    c.travelBack(25)
+    c.state(verbose = True)
+    c.travelForward(3)
+    c.state(verbose = True)
+    for i in range(0,10):
+        c.state()
+        c.nextTurn()
+    c.state(verbose = True)
